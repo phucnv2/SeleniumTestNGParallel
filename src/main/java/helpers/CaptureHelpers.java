aaf1b6b57a9c4b396d68b065c3a592350036c51e
phucnv2 <nguyenvanphuc20031996@gmail.com>
2023-07-11 00:39:46 +0700

    update

---
+++ src/main/java/helpers/CaptureHelpers.java
@@ -0,0 +1,33 @@
+package helpers;
+import drivers.DriverManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.io.FileHandler;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class CaptureHelpers {
+    //Tạo format ngày giờ để xíu gắn dô cái name của screenshot hoặc record video không bị trùng tên (không bị ghi đè file)
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH-mm-ss");
+
+    public static void captureScreenshot(String screenshotName) {
+        try {
+            // Tạo tham chiếu đối tượng của TakesScreenshot với dirver hiện tại
+            TakesScreenshot ts = (TakesScreenshot) DriverManager.getDriver();
+            // Gọi hàm getScreenshotAs để chuyển hóa hình ảnh về dạng FILE
+            File source = ts.getScreenshotAs(OutputType.FILE);
+            //Kiểm tra folder nếu không tồn tại thì tạo folder
+            File theDir = new File(SystemsHelper.getCurrentDir() + PropertiesHelper.getValue("SCREENSHOT_PATH"));
+            if (!theDir.exists()) {
+                theDir.mkdirs();
+            }
+            // Chổ này đặt tên thì truyền biến "screenName" gán cho tên File chụp màn hình
+            FileHandler.copy(source, new File(SystemsHelper.getCurrentDir() + PropertiesHelper.getValue("SCREENSHOT_PATH") + File.separator + screenshotName + "_" + dateFormat.format(new Date()) + ".png"));
+            System.out.println("Screenshot taken: " + screenshotName);
+            System.out.println("Screenshot taken current URL: " + DriverManager.getDriver().getCurrentUrl());
+        } catch (Exception e) {
+            System.out.println("Exception while taking screenshot: " + e.getMessage());
+        }
+    }
+}
--- src/main/java/helpers/ExcelHelper.java
+++ src/main/java/helpers/ExcelHelper.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelHelper {
 
@@ -144,5 +145,58 @@ public class ExcelHelper {
         } catch (Exception e) {
             e.getMessage();
         }
+    }
+    public Object[][] getExcelData(String fileName, String sheetName) {
+        Object[][] data = null;
+        Workbook workbook = null;
+        try {
+            // load the file
+            FileInputStream fis = new FileInputStream(fileName);
+
+            // load the workbook
+            workbook = new XSSFWorkbook(fis);
+
+            // load the sheet
+            Sheet sh = workbook.getSheet(sheetName);
+
+            // load the row
+            Row row = sh.getRow(0);
+
+            //
+            int noOfRows = sh.getPhysicalNumberOfRows();
+            int noOfCols = row.getLastCellNum();
+
+            System.out.println(noOfRows + " - " + noOfCols);
+
+            Cell cell;
+            data = new Object[noOfRows - 1][noOfCols];
+
+            //
+            for (int i = 1; i < noOfRows; i++) {
+                for (int j = 0; j < noOfCols; j++) {
+                    row = sh.getRow(i);
+                    cell = row.getCell(j);
+
+                    switch (cell.getCellType()) {
+                        case STRING:
+                            data[i - 1][j] = cell.getStringCellValue();
+                            break;
+                        case NUMERIC:
+                            data[i - 1][j] = String.valueOf(cell.getNumericCellValue());
+                            break;
+                        case BLANK:
+                            data[i - 1][j] = cell.getStringCellValue();
+                            break;
+                        default:
+                            data[i - 1][j] = cell.getStringCellValue();
+                            break;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("The exception is:" + e.getMessage());
+            throw new RuntimeException(e);
+        }
+        return data;
     }
 }
