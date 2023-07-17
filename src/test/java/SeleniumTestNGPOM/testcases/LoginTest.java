49fca08f197780ac5924c51c38fd22d36abe6a54
phucnv2 <nguyenvanphuc20031996@gmail.com>
2023-07-17 19:49:12 +0700

    update

--- src/test/java/SeleniumTestNGPOM/testcases/LoginTest.java
+++ src/test/java/SeleniumTestNGPOM/testcases/LoginTest.java
@@ -1,14 +1,17 @@
 package SeleniumTestNGPOM.testcases;
 
 import SeleniumTestNGPOM.common.BaseTest;
+import SeleniumTestNGPOM.listeners.TestListener;
 import SeleniumTestNGPOM.pages.login.LoginPage;
 import drivers.DriverManager;
+import helpers.CaptureHelpers;
 import helpers.ExcelHelper;
 import helpers.PropertiesHelper;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import utils.LogUtils;
-
+@Listeners(TestListener.class)
 public class LoginTest extends BaseTest {
     LoginPage loginPage; // khai báo loginPage từ package pages
 
@@ -50,6 +53,19 @@ public class LoginTest extends BaseTest {
 //        // gọi hàm login để dùng
 //        loginPage.pageLoginSuccess(PropertiesHelper.getValue("email"), PropertiesHelper.getValue("password"));
 //    }
+    @Test(priority = 6)
+    public void loginPageSuccess() {
+        //Khởi tạo đối tượng từ trang login
+        // truyền driver từ BaseTest
+//        CaptureHelpers.captureScreenshot("Phuc_123");
+        LogUtils.info("Phúc");
+        loginPage = new LoginPage();
+        ExcelHelper excelHelper = new ExcelHelper();
+        excelHelper.setExcelFile("src/test/resources/datatest/Crm.xlsx","Login");
+        // gọi hàm login để dùng
+        loginPage.pageLoginSuccess(excelHelper.getCellData("EMAIL",1), excelHelper.getCellData("PASSWORD",1));
+        excelHelper.setCellData("Pass",1,"RESULD");
+    }
     @Test(priority = 7)
     public void loginPageSuccess1() {
         //Khởi tạo đối tượng từ trang login
